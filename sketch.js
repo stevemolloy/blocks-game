b7c338f7e312f755a5f75e51829692a2e8b1e865
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2018-07-01 10:05:03 +0200

    Score generated and displayed

--- sketch.js
+++ sketch.js
@@ -1,10 +1,18 @@
+var score = 0;
+var paddle;
+var ball;
+var score_dom;
+var paused = false;
+
 function setup() {
   createCanvas(900, 600);
   paddle = new Paddle(width / 2, height - 20);
   ball = new Ball(
     createVector(width / 2, height / 2),
-    0
+    random(-3 * PI / 4, -PI / 4)
   );
+
+  score_dom = createP('Score: ' + score);
 }
 
 function draw() {
@@ -13,6 +21,30 @@ function draw() {
   paddle.show();
   ball.update();
   ball.show();
+
+  ballHitsPaddle(ball, paddle);
+
+  score_dom.html('Score: ' + score);
+}
+
+function ballHitsPaddle(ball, paddle) {
+  if (ball.pos.y < height - 30) {
+    return;
+  }
+  if (ball.pos.x < paddle.x - 75) {
+    console.log('Missed paddle on left');
+  } else if (ball.pos.x > paddle.x + 75) {
+    console.log('Missed paddle on right');
+  } else {
+    var hitPos = ball.pos.x - paddle.x;
+    ball.velocity.x += map(hitPos, -75, 75, -1, 1, true);
+    ball.velocity.y *= -1;
+    ball.velocity.normalize();
+    score++;
+    ball.speed += 0.5;
+  }
+}
+
 function togglePause() {
   if (paused) {
     paused = false;
